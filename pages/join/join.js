8688ac0cbe737d7ae62ace2b3d8125503db2851b
ccoo23 <zyf527@vip.qq.com>
2020-07-08 21:37:26 +0800

    chore(*): init project

---
+++ pages/join/join.js
@@ -0,0 +1,101 @@
+// pages/join/join.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    itemList: [
+      {
+        logo: '/images/cai.png',
+        title: '菜',
+        desc: 'absijudh hshjj as djhsj',
+      },
+      {
+        logo: '/images/fanqie.png',
+        title: '番茄',
+        desc: 'nhuu28b noob aisjd',
+      },
+      {
+        logo: '/images/xigua.png',
+        title: '西瓜',
+        desc: 'ciuwqn ioasnvopq osmnvn',
+      },
+      {
+        logo: '/images/putao.png',
+        title: '葡萄',
+        desc: 'ciuwqinisjaoi dsasddd',
+      },
+      {
+        logo: '/images/鸡.png',
+        title: '一种动物',
+        desc: 'ciuwqn 啊是大地方他忽然阿三',
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
